fb68c9933abbb1da916937454d8e66c959bec2a1
Khaos Tian <just.tzh@outlook.com>
2025-04-09 14:26:15 +0800

    更新

--- Source/Runtime/Vulkan/DescriptorPool.cpp
+++ Source/Runtime/Vulkan/DescriptorPool.cpp
@@ -1,1 +1,34 @@
-#include
+#include "Vulkan/DescriptorPool.hpp"
+#include "Vulkan/Vulkan.hpp"
+#include "Vulkan/Device.hpp"
+#include <cstdint>
+#include <vector>
+
+namespace Vulkan {
+DescriptorPool::DescriptorPool(
+    const Device&                         device,
+    const std::vector<DescriptorBinding>& descriptor_bindings,
+    size_t                                max_sets
+):
+    m_device(device) {
+    std::vector<VkDescriptorPoolSize> pool_sizes;
+
+    for (const auto& binding: descriptor_bindings) {
+        VkDescriptorPoolSize size = {
+            .type            = binding.type,
+            .descriptorCount = static_cast<uint32_t>(binding.descriptor_count * max_sets)
+        };
+        pool_sizes.emplace_back(size);
+    }
+
+    VkDescriptorPoolCreateInfo create_info = {};
+    create_info.sType                      = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    create_info.flags                      = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
+    create_info.poolSizeCount              = static_cast<uint32_t>(pool_sizes.size());
+    create_info.maxSets                    = static_cast<uint32_t>(max_sets);
+    create_info.pPoolSizes                 = pool_sizes.data();
+
+    VK_CHECK(vkCreateDescriptorPool(m_device.handle(), &create_info, nullptr, &m_handle), "create descriptor pool");
+
+} // namespace Vulkan
+} // namespace Vulkan
--- Source/Runtime/Vulkan/DescriptorPool.hpp
+++ Source/Runtime/Vulkan/DescriptorPool.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Vulkan/Vulkan.hpp"
+#include <cstddef>
 #include <cstdint>
 
 namespace Vulkan {
@@ -9,5 +10,19 @@ struct DescriptorBinding {
     uint32_t           descriptor_count;
     VkDescriptorType   type;
     VkShaderStageFlags stage;
-}
+};
+
+class Device;
+class DescriptorPool final {
+    HANDLE(VkDescriptorPool)
+private:
+    const Device& m_device;
+
+public:
+    DescriptorPool(DescriptorPool&&) = delete;
+    DescriptorPool(const Device& device, const std::vector<DescriptorBinding>& descriptor_bindings, size_t max_sets);
+    ~DescriptorPool();
+
+    const Device& device() const { return m_device; }
+};
 } // namespace Vulkan
